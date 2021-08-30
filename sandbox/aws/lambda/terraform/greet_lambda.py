e3683a6eadbddc3aeeaf309df36953a857f16519
alvaroandresgarayromero <garayromero510@gmail.com>
2021-08-30 17:43:33 -0400

    adding terraform support to deploy c++ lambda

--- sandbox/aws/lambda/terraform/greet_lambda.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def lambda_handler(event, context):
-    lambda_environment = os.environ['greeting']
-
-    message = f"{lambda_environment} from Lambda!"
-    return {'message': message}
